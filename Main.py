9c564361294bd853ab368852f5883fbb85cd344e
tuncay98 <37210254+tuncay98@users.noreply.github.com>
2018-12-09 23:59:46 +0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import random
+import ProxyChange
+
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+
+#file2 = open("prx.txt", "r")
+#Numbers = file2.read().splitlines()
+capa = DesiredCapabilities.CHROME
+capa["pageLoadStrategy"] = "none"
+path = "C:\chromedriver.exe"
+
+
+proxy = random.choice(list(ProxyChange.get_proxies()))
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--proxy-server=%s' % proxy)
+browser = webdriver.Chrome(path, chrome_options=chrome_options, desired_capabilities=capa)
+browser.delete_all_cookies()
+wait = WebDriverWait(path, 20)
+browser.get("https://www.google.com.tr")
